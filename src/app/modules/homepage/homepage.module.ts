cc404159c162f111a1d47d7bc4dd8ee2edfd8ad1
Vinit Soni <vinitsoni9869@gmail.com>
2018-11-08 21:42:17 -0500

    start landing page

--- src/app/modules/homepage/homepage.module.ts
+++ src/app/modules/homepage/homepage.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
 import { HomepageComponent } from "./components/homepage.component";
 import { homepageRouting } from "./homepage-routing.module";
+import { Header1Component } from "../ui/header1/header1.component";
+import { AnchorComponent } from "../ui/anchor/anchor.component";
 
 @NgModule({
-  declarations: [ HomepageComponent ],
+  declarations: [
+    AnchorComponent,
+    Header1Component,
+    HomepageComponent
+  ],
   imports: [ homepageRouting ]
 })
 
