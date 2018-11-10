0e975b41e0a0d1d6ed21779d578ae6f24f98573e
Vinit Soni <vinitsoni9869@gmail.com>
2018-11-10 16:11:55 -0500

    add mobile layout support

--- src/app/modules/homepage/components/homepage.component.ts
+++ src/app/modules/homepage/components/homepage.component.ts
@@ -10,7 +10,9 @@ import { Component, OnInit } from "@angular/core";
 
 export class HomepageComponent implements OnInit {
 
-  private newUsernameValue: String = "asds";
+  private newFullNameValue: String = "";
+  private newUsernameValue: String = "";
+  private newPhoneValue: String = "";
   
   ngOnInit() {
 
