ff3f04c45fae0ff802340ca6304311a0e5d15222
Vinit Soni <vinitsoni9869@gmail.com>
2019-01-07 14:38:26 -0500

    clean up game-session.component

--- src/app/modules/game-session/components/game-session/game-session.component.ts
+++ src/app/modules/game-session/components/game-session/game-session.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnDestroy } from "@angular/core";
-import { Observable, Subscriber, Subscription } from "rxjs";
-import { Store, createSelector, select } from "@ngrx/store";
+import { Observable, Subscription } from "rxjs";
+import { Store, select } from "@ngrx/store";
 import { SetUsername } from "../../store/user/user.actions";
-import { SetGameSessionName, GetServerGameSession } from "../../../../store/game-session/game-session.actions";
-import { GameSession } from "../../../../types/game-session/game-session.interface";
+import { GetServerGameSession } from "../../../../store/game-session/game-session.actions";
 import { ActivatedRoute, Router } from "@angular/router";
 import { AppState } from "../../../../types/app-state/app-state.interface";
-import { tap } from "rxjs/operators";
 
 import * as gameSessionDataSelectors from "../../store/game-session-data/game-session-data.selectors";
+import * as gameSessionSelectors from "../../../../store/game-session/game-session.selectors";
 
 @Component({
   selector: "game-session",
@@ -17,34 +16,22 @@ import * as gameSessionDataSelectors from "../../store/game-session-data/game-se
 })
 
 export class GameSessionComponent implements OnDestroy {
-
-  private usernameStr: string;
-
-  private gameSession$: Observable<GameSession>;
   private gameSessionName: string;
-  private gameSessionSubscription: Subscription;
+  private gameSessionIsFreeSubscription: Subscription;
   private currentUserUsername$: Observable<string>;
+  private gameSessionIsFree$: Observable<boolean>;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private store: Store<AppState>
   ) {
-    this.store.pipe(
-      select(createSelector((state: AppState) => state)),
-      tap(a => a)
-    ).subscribe(a => console.log(a));
-
     this.store.dispatch(new GetServerGameSession(this.route.snapshot.params['gameSessionName']));
-    this.gameSession$ = this.store.select('gameSession');
     this.currentUserUsername$ = this.store.pipe(select(gameSessionDataSelectors.getUserUsername));
-    this.gameSessionSubscription = this.gameSession$
-      .subscribe(gS => {
-        if (!gS.isGameSessionFree) {
-          this.router.navigate([this.router.url + '/in-progress'])
-          return;
-        }
-        this.gameSessionName = gS.name;
+    this.gameSessionIsFree$ = this.store.pipe(select(gameSessionSelectors.getGameSessionIsFree));
+    this.gameSessionIsFreeSubscription = this.gameSessionIsFree$
+      .subscribe(isFree => {
+        if (!isFree) this.router.navigate([this.router.url + '/in-progress'])
       });
   }
 
@@ -53,7 +40,7 @@ export class GameSessionComponent implements OnDestroy {
   }
 
   ngOnDestroy() {
-    this.gameSessionSubscription.unsubscribe();
+    this.gameSessionIsFreeSubscription.unsubscribe();
   }
 
 
