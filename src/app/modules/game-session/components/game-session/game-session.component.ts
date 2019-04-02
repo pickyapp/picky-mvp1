ac15e441f9eee758cd8bae9215a2aa9551c2744f
Vinit Soni <vinitsoni9869@gmail.com>
2019-04-02 10:52:32 -0400

    Added polling for gs updates

--- src/app/modules/game-session/components/game-session/game-session.component.ts
+++ src/app/modules/game-session/components/game-session/game-session.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnDestroy } from "@angular/core";
-import { Observable, Subscription, interval } from "rxjs";
-import { shareReplay, switchMap, tap } from 'rxjs/operators';
+import { interval, of, Subject, Subscription } from "rxjs";
+import { shareReplay, switchMap, tap, takeWhile } from 'rxjs/operators';
 import { ActivatedRoute, Router } from "@angular/router";
-import { AppState } from "../../../../types/app-state/app-state.interface";
 
-import { GameSession } from "src/app/types/game-session/game-session.interface";
 import { CookieService } from "ngx-cookie-service";
 import { UserService } from "../../services/user.service";
 import { GameSessionService } from "src/app/services/game-session.service";
@@ -15,17 +13,20 @@ import { GameSessionService } from "src/app/services/game-session.service";
   styleUrls: [ "game-session.component.scss" ]
 })
 
-export class GameSessionComponent {
-  
-  private isAddUserDisabled: boolean = false;
+export class GameSessionComponent implements OnDestroy {
+
   private addButtonText: string = "Add me to the game";
   private gameSessionName: string;
-  private routeSub: Subscription;
+
+  private routeSubscription: Subscription;
+  private pollSubscription: Subscription;
   
   // 's' prepended vars are vars related to the current cookie session
   private sCurrUser;
+  private sCurrUser$: Subject<object>;
   private sAllUsers: string[];
   private sCurrGameSession;
+  private sCurrGameSession$: Subject<object>;
 
   constructor(
     private route: ActivatedRoute,
@@ -34,45 +35,42 @@ export class GameSessionComponent {
     private userService: UserService,
     private gsService: GameSessionService
   ) {
-    this.routeSub = this.route.params.subscribe(params => {
-      this.gameSessionName = params["gameSessionName"];
-      this.gsService.makeSession(this.gameSessionName).subscribe(r => {
-        this.updateFromCookieSession();
-        if (!this.sCurrGameSession.isGameSessionFree) {
-          this.router.navigate(['in-progress'], { relativeTo: this.route });
-        }
-        // if (this.sCurrUser) this.isAddUserDisabled = true; FIXME uncomment
-
-      });
-      // TODO: need to only make session once, subscription might be 
-      // invoked multiple times
-
+    this.sCurrUser$ = new Subject<object>();
+    this.sCurrGameSession$ = new Subject<object>();
+    this.routeSubscription = this.route.params.pipe(
+      takeWhile(val => !this.gameSessionName), // while doesn't exist
+      tap((params) => {
+        this.gameSessionName = params["gameSessionName"];
+        return params;
+      }),
+      switchMap((params) => this.gsService.makeSession(this.gameSessionName)),
+    ).subscribe(resp => {
+      this.updateFromCookieSession();
+      if (!this.sCurrGameSession.isGameSessionFree) {
+        this.router.navigate(['in-progress'], { relativeTo: this.route });
+      }
     });
-  }
-
-  startPoller1() {
-    var a = interval(2000).pipe(
-      switchMap(() => this.gsService.getSessionAt(this.gameSessionName)),
-      tap((resp) => {
-        console.log(resp);
+    this.pollSubscription = interval(2000).pipe( // Polling for updates
+      shareReplay(),
+      switchMap(() => this.gsService.getSessionAt(this.gameSessionName))
+      ).subscribe((resp) => {
         this.updateFromCookieSession();
         return resp;
-      }),
-      shareReplay());
-      a.subscribe(e => console.log("Subs: ", e));
+    });
   }
 
   updateFromCookieSession() {
     var this_user = JSON.parse(atob(this.cookieService.get("user")));
-    this.sCurrUser = this_user["user"] ? this_user["user"] : undefined;
+    this.sCurrUser = this_user["user"];
     this.sCurrGameSession = this_user["game_session"];
-    this.sAllUsers = this_user["all_users"];
+    this.sAllUsers = this_user["game_session"]["all_users"];
+    this.sCurrUser$.next(this.sCurrUser);
+    this.sCurrGameSession$.next(this.sCurrGameSession);
   }
 
   setUsername(val: string) {
     // this.isAddUserDisabled = true; FIXME uncomment
     this.addButtonText = "Added!"
-    console.log("Adding: ", val, this.gameSessionName);
     this.userService.setUsername(val, this.gameSessionName).subscribe(resp => {
       this.updateFromCookieSession();
     });
@@ -87,7 +85,10 @@ export class GameSessionComponent {
     console.log(this_user);
   }
 
-
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
+    this.pollSubscription.unsubscribe();
+  }
   
 }
 
