08cd9fe2bdd60f30b60a4054093a86db9173beaf
Vinit Soni <vinitsoni9869@gmail.com>
2019-06-04 21:17:35 -0400

    add live to game session urls (#21)

--- src/app/modules/game-session/game-session-routing.module.ts
+++ src/app/modules/game-session/game-session-routing.module.ts
@@ -3,8 +3,8 @@ import { GameSessionComponent } from "./components/game-session/game-session.com
 import { InProgressComponent } from './components/in-progress/in-progress.component';
 
 const routes: Routes = [
-  { path: '', component: GameSessionComponent },
-  { path: 'in-progress', component: InProgressComponent }
+  { path: ':gameSessionName', component: GameSessionComponent },
+  { path: ':gameSessionName/in-progress', component: InProgressComponent }
 ];
 
 export const gameSessionRouting = RouterModule.forChild(routes);
