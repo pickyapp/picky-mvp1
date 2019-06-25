6a1a897d253edee153997e6ffa0905cce7f3b9e5
Vinit Soni <vinitsoni9869@gmail.com>
2019-06-25 16:36:31 -0400

    added room page

--- src/app/modules/room/room.module.ts
+++ src/app/modules/room/room.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
 import { RoomRouting } from "./room-routing.module";
 import { RoomCreateComponent } from "./components/room-create.component";
+import { ClickButtonComponent } from "../ui/click-button/click-button.component";
+import { UiModule } from "../ui/ui.module";
+import { RoomService } from "./services/room.service";
 
 
 
@@ -9,9 +12,13 @@ import { RoomCreateComponent } from "./components/room-create.component";
     RoomCreateComponent
   ],
   imports: [
-    RoomRouting
+    RoomRouting,
+    UiModule
   ],
-  exports: []
+  exports: [],
+  providers: [
+    RoomService
+  ]
 })
 
 export class RoomModule { }
