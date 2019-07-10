355f6391c11ac7a8a85531761776cb41ea0a193e
Vinit Soni <vinitsoni9869@gmail.com>
2019-07-10 11:24:29 -0400

    fix questionText username not changing in answers

--- src/app/modules/ui/answer/answer.component.ts
+++ src/app/modules/ui/answer/answer.component.ts
@@ -12,11 +12,16 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 export class AnswerComponent {
   @Input("question") question;
   @Input("buddyName") buddyName: string;
+  @Input("username") username: string;
   @Input("optionSelected") optionSelected: number;
   @Output('nextClick') nextClick = new EventEmitter<boolean>();
 
 
   onNext() {
     this.nextClick.emit(true);
+  }
+
+  ngOnChanges() {
+    this.question.questionText = this.question.questionText.replace("{USER}", this.username);
   }
 }
