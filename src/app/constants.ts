14021733e374e78b1c24f03e8a61f4fc8ccb258a
Vinit Soni <vinitsoni9869@gmail.com>
2019-07-10 15:36:41 -0400

    update colour schemes for buttons and question/answer components (#27)
    * update colour schemes for buttons and question/answer components
    
    * fix undefined var error

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,9 +4,9 @@ export const primaryLightColour: string = "#9162e4";
 export const primaryDarkColour: string = "#280680";
 export const btnSelectedColour: string = "#000063";
 
-export const optionBtnColour: string = "#ffa270";
-export const optionBtnClickSelected: string = "#c41c00"; // Click Selected: for a brief moment when clicked
-export const optionBtnSelected: string = "#00e676";      // Selected: indefinitely selected (i.e. buddy answer) 
+export const optionBtnColour: string = "linear-gradient(-45deg, #ffa06d, #ff79b0)";
+export const optionBtnClickSelected: string = "linear-gradient(-45deg, #c41c00, #c41c00)"; // Click Selected: for a brief moment when clicked
+export const optionBtnSelected: string = "linear-gradient(-45deg, #00e676, #00e676)";      // Selected: indefinitely selected (i.e. buddy answer) 
 
-export const disabledColour: string ="#a4a4a4";
+export const disabledColour: string ="linear-gradient(-45deg, #a4a4a4, #a4a4a4)";
 
